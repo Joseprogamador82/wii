c2a61316f4a7f3319ec4cef149c2bfe5fa72c3ba
Joseprogamador82 <100492156+Joseprogamador82@users.noreply.github.com>
2022-02-27 11:45:22 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -617,7 +617,7 @@ class PlayState extends MusicBeatState
 					balloons.setGraphicSize(Std.int(balloons.width * 0));
 					balloons.x = -580;
 					balloons.y = -200;
-					balloons.visible = true;.
+					balloons.visible = true;
 					}
 			case 'school':
 			{
