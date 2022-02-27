5a76827b2f2810bda4c9fb9a8d24a2eff3ef4687
Joseprogamador82 <100492156+Joseprogamador82@users.noreply.github.com>
2022-02-27 11:38:45 -0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -501,41 +501,7 @@ class PlayState extends MusicBeatState
 					limo.antialiasing = true;
 
 					fastCar = new FlxSprite(-300, 160).loadGraphic(Paths.image('limo/fastCarLol','week4'));
-			}              // add(limo);
-			case 'diningarea':
-			{
-					curStage = 'diningarea;
-
-					defaultCamZoom = 0.8;
-
-					var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image(''freddystage/stagecurtains','shared'));
-					bg.antialiasing = true;
-					bg.scrollFactor.set(0.2, 0.2);
-					bg.active = false;
-					bg.setGraphicSize(Std.int(bg.width * 0.8));
-					bg.updateHitbox();
-					add(bg);
-
-					balloons = new FlxSprite().loadGraphic(Paths.image('freddystage/stagecurtains','shared'));
-					balloons.setGraphicSize(Std.int(balloons.width * 0));
-					balloons.x = -580;
-					balloons.y = -200;
-					balloons.visible = true;
-					
-
-					balloons = new FlxSprite().loadGraphic(Paths.image('freddystage/stagecurtains','shared'));
-					balloons.setGraphicSize(Std.int(balloons.width * 0));
-					balloons.x = -580;
-					balloons.y = -200;
-					balloons.visible = true;
-					
-					
-					balloons = new FlxSprite().loadGraphic(Paths.image('freddystage/stagecurtains','shared'));
-					balloons.setGraphicSize(Std.int(balloons.width * 0));
-					balloons.x = -580;
-					balloons.y = -200;
-					balloons.visible = true;		
-		
+					// add(limo);
 			}
 			case 'mall':
 			{
@@ -620,6 +586,38 @@ class PlayState extends MusicBeatState
 					var evilSnow:FlxSprite = new FlxSprite(-200, 700).loadGraphic(Paths.image("christmas/evilSnow",'week5'));
 						evilSnow.antialiasing = true;
 					add(evilSnow);
+					}
+			// WITHERED FREDDY STAGE 1
+			case 'diningarea':
+				{
+					defaultCamZoom = 0.8;
+					curStage = 'diningarea';
+					var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('freddystage/stageback','shared'));
+					bg.setGraphicSize(Std.int(bg.width / 0.9));
+					
+					bg.x = -600; {
+					bg.y = -200;
+					add(bg);
+
+					balloons = new FlxSprite().loadGraphic(Paths.image('freddystage/stagecurtains','shared'));
+					balloons.setGraphicSize(Std.int(balloons.width * 0));
+					balloons.x = -580;
+					balloons.y = -200;
+					balloons.visible = true;
+					
+
+					balloons = new FlxSprite().loadGraphic(Paths.image('freddystage/stagecurtains','shared'));
+					balloons.setGraphicSize(Std.int(balloons.width * 0));
+					balloons.x = -580;
+					balloons.y = -200;
+					balloons.visible = true;
+					
+					
+					balloons = new FlxSprite().loadGraphic(Paths.image('freddystage/stagecurtains','shared'));
+					balloons.setGraphicSize(Std.int(balloons.width * 0));
+					balloons.x = -580;
+					balloons.y = -200;
+					balloons.visible = true;.
 					}
 			case 'school':
 			{
@@ -774,6 +772,41 @@ class PlayState extends MusicBeatState
 						stageCurtains.active = false;
 	
 						add(stageCurtains);
+								// add(limo);
+			}
+			case 'diningarea':
+			{
+					curStage = 'diningarea;
+
+					defaultCamZoom = 0.8;
+
+					var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image(''freddystage/stagecurtains','shared'));
+					bg.antialiasing = true;
+					bg.scrollFactor.set(0.2, 0.2);
+					bg.active = false;
+					bg.setGraphicSize(Std.int(bg.width * 0.8));
+					bg.updateHitbox();
+					add(bg);
+
+					balloons = new FlxSprite().loadGraphic(Paths.image('freddystage/stagecurtains','shared'));
+					balloons.setGraphicSize(Std.int(balloons.width * 0));
+					balloons.x = -580;
+					balloons.y = -200;
+					balloons.visible = true;
+					
+
+					balloons = new FlxSprite().loadGraphic(Paths.image('freddystage/stagecurtains','shared'));
+					balloons.setGraphicSize(Std.int(balloons.width * 0));
+					balloons.x = -580;
+					balloons.y = -200;
+					balloons.visible = true;
+					
+					
+					balloons = new FlxSprite().loadGraphic(Paths.image('freddystage/stagecurtains','shared'));
+					balloons.setGraphicSize(Std.int(balloons.width * 0));
+					balloons.x = -580;
+					balloons.y = -200;
+					balloons.visible = true;
 				}
 			default:
 			{
